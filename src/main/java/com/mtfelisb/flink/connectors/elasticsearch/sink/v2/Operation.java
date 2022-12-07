5526091c70877c2d7f45d41105a13417f2e991f9
Matheus Felisberto <matheus.felisberto@hotmail.com>
2022-12-07 02:33:06 +0000

    add: sink v2 operator

---
+++ src/main/java/com/mtfelisb/flink/connectors/elasticsearch/sink/v2/Operation.java
@@ -0,0 +1,38 @@
+/*
+ *
+ *  * Licensed to the Apache Software Foundation (ASF) under one
+ *  * or more contributor license agreements.  See the NOTICE file
+ *  * distributed with this work for additional information
+ *  * regarding copyright ownership.  The ASF licenses this file
+ *  * to you under the Apache License, Version 2.0 (the
+ *  * "License"); you may not use this file except in compliance
+ *  * with the License.  You may obtain a copy of the License at
+ *  *
+ *  * http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing,
+ *  * software distributed under the License is distributed on an
+ *  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  * KIND, either express or implied.  See the License for the
+ *  * specific language governing permissions and limitations
+ *  * under the License.
+ *
+ */
+
+package com.mtfelisb.flink.connectors.elasticsearch.sink.v2;
+
+import co.elastic.clients.elasticsearch.core.bulk.BulkOperation;
+import co.elastic.clients.elasticsearch.core.bulk.BulkOperationVariant;
+import java.io.Serializable;
+
+public class Operation implements Serializable {
+    private final BulkOperation bulkOperation;
+
+    public Operation(BulkOperationVariant bulkOperationVariant) {
+        this.bulkOperation = bulkOperationVariant._toBulkOperation();
+    }
+
+    public BulkOperation getBulkOperation() {
+        return bulkOperation;
+    }
+}
