671eee5b6f832ab01d2bf004ecdbb368ffb3bd4c
Matheus Felisberto <matheus.felisberto@hotmail.com>
2022-12-15 00:24:06 +0000

    add: operation serializer using kryo

---
+++ src/main/java/com/mtfelisb/flink/connectors/elasticsearch/sink/v2/OperationSerializer.java
@@ -0,0 +1,60 @@
+/*
+ *
+ *  * Licensed to the Apache Software Foundation (ASF) under one
+ *  * or more contributor license agreements.  See the NOTICE file
+ *  * distributed with this work for additional information
+ *  * regarding copyright ownership.  The ASF licenses this file
+ *  * to you under the Apache License, Version 2.0 (the
+ *  * "License"); you may not use this file except in compliance
+ *  * with the License.  You may obtain a copy of the License at
+ *  *
+ *  * http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing,
+ *  * software distributed under the License is distributed on an
+ *  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  * KIND, either express or implied.  See the License for the
+ *  * specific language governing permissions and limitations
+ *  * under the License.
+ *
+ */
+
+package com.mtfelisb.flink.connectors.elasticsearch.sink.v2;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+import org.objenesis.strategy.StdInstantiatorStrategy;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+
+public class OperationSerializer {
+    private final Kryo kryo = new Kryo();
+
+    public OperationSerializer() {
+        kryo.setRegistrationRequired(false);
+        kryo.setInstantiatorStrategy(new StdInstantiatorStrategy());
+    }
+
+    public void serialize(Operation request, DataOutputStream out) {
+        Output output = new Output(out);
+        kryo.writeObject(output, request);
+        output.flush();
+    }
+
+    public Operation deserialize(long requestSize, DataInputStream in) {
+        Input input = new Input(in, (int) requestSize);
+        return kryo.readObject(input, Operation.class);
+    }
+
+    public int size(Operation operation) {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        Output output = new Output(byteArrayOutputStream);
+        kryo.writeObject(output, operation);
+        output.flush();
+
+        return (int) output.total();
+    }
+}
